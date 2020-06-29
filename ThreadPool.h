b3b58071bd0087d876b1aba0569023355f4c14fd
Yuxi <SuYuxi@users.noreply.github.com>
2020-06-30 00:39:51 +0900

    Update ThreadPool.h

--- ThreadPool.h
+++ ThreadPool.h
@@ -7,8 +7,6 @@
 #include <queue>
 #include <memory>
 
-#include <iostream>
-
 class ThreadPool {
 public:
 	ThreadPool(int _threadNum = 4);
@@ -17,15 +15,18 @@ public:
 	void pause();
 	void resume();
 	void shutdown();
+	void waitAllFinish();
 
-template<class Func, class... Args>
-auto add(Func&& func, Args&& ...args)
-	->std::future<typename std::result_of<Func(Args...)>::type>;
+	template<class Func, class... Args>
+	auto add(Func&& func, Args&& ...args)
+		->std::future<typename std::result_of<Func(Args...)>::type>;
 
 private:
 	int threadNum;
 	bool suspend;
 	bool stop;
+	int32_t threadState; // bit 1 means thread is in use, bit 0 means thread is idle.
+
 	std::mutex taskMutex;
 	std::condition_variable cv;
 
@@ -34,36 +35,62 @@ private:
 };
 
 ThreadPool::ThreadPool(int _threadNum)
-	: threadNum(_threadNum), stop(false), suspend(false)
+	: threadNum(_threadNum), stop(false), suspend(false), threadState(0)
 {
-	std::function<void()> threadFunc = [this]() {
+	std::function<void(int)> threadFunc = [this](int id) {
 		while (true)
 		{
 			std::function<void()> task;
 			{
 				std::unique_lock<std::mutex> lock(this->taskMutex);
-				this->cv.wait(lock, [this] {return !this->taskQueue.empty() || this->stop; });
+				this->cv.wait(lock, [this] {return this->stop || (!this->suspend && !this->taskQueue.empty()); });
 				if (this->stop && this->taskQueue.empty()) return;
 				task = std::move(taskQueue.front());
 				this->taskQueue.pop();
 			}
+			threadState |= (1 << id);
 			task();
+			threadState &= ~(1 << id);
+			cv.notify_one();
 		}
 	};
 	for (int i = 0; i < threadNum; ++i)
 	{
-		threadList.emplace_back(threadFunc);
+		threadList.emplace_back(threadFunc, i);
 	}
 }
 
 ThreadPool::~ThreadPool()
 {
-	shutdown();
+	stop = true;
 	cv.notify_all();
 	for (std::thread& t : threadList)
 	{
 		t.join();
 	}
+}
+
+inline void ThreadPool::pause()
+{
+	suspend = true;
+}
+
+inline void ThreadPool::resume()
+{
+	suspend = false;
+	cv.notify_all();
+}
+
+inline void ThreadPool::shutdown()
+{
+	stop = true;
+	cv.notify_all();
+}
+
+inline void ThreadPool::waitAllFinish()
+{
+	std::unique_lock<std::mutex> lock(taskMutex);
+	cv.wait(lock, [this] { return !this->threadState && this->taskQueue.empty(); });
 }
 
 template<class Func, class... Args>
@@ -81,21 +108,6 @@ auto ThreadPool::add(Func&& func, Args&& ...args)
 		if (stop) throw std::runtime_error("Add task to stopped threadPool");
 		taskQueue.emplace([task](){ (*task)(); });
 	}
-  cv.notify_one();
+	cv.notify_one();
 	return res;
-}
-
-void ThreadPool::pause()
-{
-	suspend = true;
-}
-
-void ThreadPool::resume()
-{
-	suspend = true;
-}
-
-void ThreadPool::shutdown()
-{
-	stop = true;
 }
